95cba17fe1ce3e15261869f27793ec2601904a9f
leria-kosse <leria.kosse@gmail.com>
2026-01-30 11:08:29 -0600

    FizzBuzz program completed

--- src/main/java/edu/grinnell/csc207/exploration/FizzBuzz.java
+++ src/main/java/edu/grinnell/csc207/exploration/FizzBuzz.java
@@ -7,7 +7,21 @@ public class FizzBuzz {
     //
     // mvn compile exec:java -q "-Dexec.mainClass=edu.grinnell.csc207.exploration.FizzBuzz"
     public static void main(String[] args) {
-        // TODO: fill me in!
-        System.out.println("Hello world!");   
+        for (int i = 1; i <= Integer.parseInt(args[0]); i++){
+            if(((i % 3) == 0) && ((i % 5) == 0)) {
+                System.out.println("fizzbuzz");
+            }
+            else if((i % 3) == 0){
+                System.out.println("fizz");
+            }
+            else if((i % 5) == 0){
+                System.out.println("buzz");
+            }
+            else
+            {
+                System.out.println(i);  
+            }
+            
+        }
     }
 }
